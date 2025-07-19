0bcb38715b991aed961f91a244dd153ebd41fe0e
Eduardo <msdohko@gmail.com>
2025-07-19 03:01:47 -0300

    primeiro commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,48 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner opcoes = new Scanner(System.in);
+        String nome = "Eduardo Hoiser";
+        String conta = "Corrente";
+        double saldoConta = 2200;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // inicializar dados do cliente
+        System.out.println("************************************************");
+        System.out.println("\nNome do cliente: "+nome);
+        System.out.println("Tipo de conta: "+conta);
+        System.out.println("Seu saldo: R$ "+saldoConta);
+        System.out.println("\n************************************************");
+
+        // menu de opções
+        System.out.println("Operações:");
+        System.out.println("1- Consultar saldos");
+        System.out.println("2- Receber valor");
+        System.out.println("3- Transferir valor");
+        System.out.println("4- Sair");
+        System.out.println("Digite a opção desejada:");
+        int opcao = opcoes.nextInt();
+
+        if (opcao == 1) {
+            System.out.println("Seu saldo é "+saldoConta);
+        } if (opcao == 2) {
+            System.out.println("Digite um valor para deposito:");
+            double deposito = opcoes.nextDouble();
+            double saldoTotal = deposito + saldoConta;
+            System.out.println("Seu saldo atual é: "+saldoTotal);
+        }if (opcao == 3) {
+            System.out.println("Digite o valor para transferencia:");
+            double transferencia = opcoes.nextDouble();
+            if (transferencia > saldoConta) {
+                System.out.println("Saldo insuficiente!");
+            } else {
+                double saldoTotal = saldoConta - transferencia;
+                System.out.println("Seu saldo atual é: "+saldoTotal);
+            }
+        }if (opcao == 4) {
+            System.out.println("Operação encerrada!");
+        }
         }
     }
-}
